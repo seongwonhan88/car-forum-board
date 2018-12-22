15bbb455775c368f4639a9e1832df527302c3c03
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-23 00:39:56 +0900

    board model update CarTalkBoard, CarBrands

--- app/board/models.py
+++ app/board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class CarTalkBoard(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    view_count = models.PositiveIntegerField()
+    content = models.TextField()
+
+
+class CarBrands(models.Model):
+    pass

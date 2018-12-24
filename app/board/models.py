d7050c73a8ab9817b024d6b653da69d52140a366
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-24 14:03:40 +0900

    BrandTag added

--- app/board/models.py
+++ app/board/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-class CarTalkBoard(models.Model):
+class DomesticCarTalkBoard(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False)
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now_add=True)
     view_count = models.PositiveIntegerField()
     content = models.TextField()
+    tags = models.ManyToManyField('BrandTags', related_name='boards')
 
 
-class CarBrands(models.Model):
-    pass
+class BrandTags(models.Model):
+    company = models.CharField(max_length=200)
+    brand_logo = models.ImageField(upload_to='logo')

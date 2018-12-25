5f697e77906b026d29acd3c5aab0f03a65ff512f
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-26 01:51:44 +0900

    Board models added. Comment/PostImages

--- app/board/models.py
+++ app/board/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
 
 class DomesticCarTalkBoard(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False)
@@ -6,9 +10,24 @@ class DomesticCarTalkBoard(models.Model):
     modified_at = models.DateTimeField(auto_now_add=True)
     view_count = models.PositiveIntegerField()
     content = models.TextField()
-    tags = models.ManyToManyField('BrandTags', related_name='boards')
+    is_notification = models.BooleanField(default=False)
 
+    # relational model fields
+    tags = models.ManyToManyField('BrandTags', related_name='boards')
+    users = models.ManyToManyField('User', related_name='posts')
 
 class BrandTags(models.Model):
-    company = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
     brand_logo = models.ImageField(upload_to='logo')
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    user = models.ManyToManyField('User', related_name='comments')
+
+
+class PostImages(models.Model):
+    post_id = models.ForeignKey('DomesticCarTalkBoard')
+    image = models.ImageField(upload_to='post')

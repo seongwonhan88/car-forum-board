fe836b272624c0ffb6240ef44efb1abac2947ae1
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-26 23:49:54 +0900

    User model field added

--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     img_profile = models.ImageField(upload_to='users')
-
-    class Meta:
-        abstract = True
+    nickname = models.CharField(max_length=100, null=True, blank=True)

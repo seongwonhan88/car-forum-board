ea4f97d60970f3b728f093a9de23b8aa8b34f737
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-28 01:15:08 +0900

    user model admin register, user property setup for default image

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+
+admin.site.register(User, UserAdmin)
--- app/members/models.py
+++ app/members/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.db import models
 
 
 class User(AbstractUser):
     img_profile = models.ImageField(upload_to='users')
     nickname = models.CharField(max_length=100, null=True, blank=True)
+
+    @property
+    def get_image_url(self):
+        if self.img_profile:
+            return self.img_profile.url
+        return static('images/blank_user.png')
+
+    class Meta:
+        verbose_name = '사용자'
+        verbose_name_plural = f'{verbose_name} 목록'

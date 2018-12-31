2b073c9a507843d9937654a6478070a0ef4545b2
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-31 18:50:54 +0900

    login and logout views added

--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            login(request, form.user)
+            next = request.GET.get('next')
+            if next:
+                return redirect(next)
+            return redirect('board:post-list')
+    else:
+        form = LoginForm()
+    context['form'] = form
+    return render(request, 'members/login.html', context)
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('board:post-list')
+    else:
+        pass
